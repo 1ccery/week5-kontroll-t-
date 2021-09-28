6f64b615c82dde49389dd269bbc2c0eaa7af704e
Opilane TTHK <opilane@tthk.ee>
2021-09-28 13:57:28 +0300

    Kontroll-töö

---
+++ PositiveNegative/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PositiveNegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            /*The program checks if the number is positive or negative*/
+            Console.WriteLine("Enter your number: ");
+            int userNumber;
+            userNumber = Convert.ToInt32(Console.ReadLine());
+            
+            
+
+            
+            if (userNumber > 0)
+            {
+                Console.WriteLine("Positiivne");
+            }
+            else if (userNumber < 0)
+            {
+                Console.WriteLine("Negatiivne");
+
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
